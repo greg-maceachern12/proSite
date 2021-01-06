534fd52aa7c2caeee9fda0db4c6c5410f7d1d0bb
Greg <g.maceachern@queensu.ca>
2021-01-05 19:44:54 -0700

    first

---
+++ js/prof.js
@@ -0,0 +1,41 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+const form = document.getElementById("contact-form"); 
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+})
+const sendMail = (mail) => {
+  //1.
+  fetch("https://gregmaceachern.com/send", {
+    method: "post", //2.
+    body: mail, //3.
+
+  }).then((response) => {
+    return response.json();
+  });
+};
